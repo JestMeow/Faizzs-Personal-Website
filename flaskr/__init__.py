d4788e5e243f1c8160daa43a1127d6ac1593e75f
JestMeow <theweirdboi901@gmail.com>
2026-07-11 00:00:43 +0800

    Some minor changes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,7 +4,6 @@ from .home import app
 print("Static folder:", app.static_folder)
 print("Template folder:", app.template_folder)
 
-
 def create_app():
     flask_app = Flask(__name__)
     flask_app.register_blueprint(app)
